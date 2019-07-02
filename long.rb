a1ae306166bbb7af9fb506c5d5b3d79452f4b34b
vanlongvl99 <nguyenvanlongt2@gmail.com>
2019-07-02 17:47:14 +0700

    long.rb

---
+++ long.rb
@@ -0,0 +1,100 @@
+
+num_0 = [" _ _ ",
+    "|   |",
+    "|_ _|"]
+num_1 = ["     ",
+    " /|  ",
+    "  |  "]
+num_2 = [" _ _ ",
+    " _ _|",
+    "|_ _ "]
+num_3 = ["_ _  ",
+    "_ _| ",
+    "_ _| "]
+num_4 = ["     ",
+    "|_ _|",
+    "    |"]
+num_5 = [" _ _ ",
+    "|_ _ ",
+    " _ _|"]
+num_6 = [" _ _ ",
+    "|_ _ ",
+    "|_ _|"]
+num_7 = ["_ _  ",
+    "  /  ",
+    " /   "]
+num_8 = [" _ _ ",
+    "|_ _|",
+    "|_ _|"]
+num_9 = [" _ _ ",
+    "|_ _|",
+    " _ _|"]
+symbol =["     ",
+    "  %  ",
+    "  %  "]
+
+font = [num_0, num_1, num_2, num_3, num_4, num_5, num_6, num_7, num_8, num_9, symbol]
+
+#input: clock of system
+#output:string clock
+def get_string_clock_of_system
+    time_now=Time.now.to_s
+    time_string=""
+    for i in 11..19 do
+        time_string+=time_now[i]
+    end
+    return time_string
+end
+
+#input: 1 character and font
+#output: arr_font of character
+def convert_1_character_to_1_font(character,font)
+    numbers="0123456789" #array to check number
+    lc=0                 # flag
+    for i in 0..9 do
+        if character==numbers[i]
+            lc=1
+        end
+    end
+    if lc==1
+        return font[character.to_i]
+    else
+        return font[-1]
+    end 
+    
+end
+
+#input: string and font
+#output: array of font
+def convert_time_string_to_array_font (time_string,font) 
+    array=[]
+    for i in 0..(time_string.length-2) do
+        convert=convert_1_character_to_1_font(time_string[i],font)
+        array.push(convert)
+    end
+    return array
+    
+end
+
+def merge_arr (arr)
+    merge=["","","","","","","","","",""]
+    for i in 0..(arr[0].length-1) do    
+        for j in 0..(arr.length-1) do
+            font=arr[j]
+            merge[i]+=font[i]
+        end
+    end
+    return merge
+end
+
+pre_sec=0
+while true
+    system("clear")
+    time_now=Time.now
+    time_string=get_string_clock_of_system
+    convert=convert_time_string_to_array_font(time_string,font)
+    merge=merge_arr(convert)
+    puts merge
+    sleep(1)    
+end
+
